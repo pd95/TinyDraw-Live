562c7ee3f977f41973128658fe33000978c8c1a8
Philipp <pd95@users.noreply.github.com>
2021-12-11 20:14:19 +0100

    Remove own NavigationView and navigation title (which overlayed the file name)

--- TinyDraw/ContentView.swift
+++ TinyDraw/ContentView.swift
@@ -14,75 +14,71 @@ struct ContentView: View {
     @State private var showingBrushOptions = false
 
     var body: some View {
-        NavigationView {
-            Canvas { context, size in
-                // drawing here
-                for stroke in drawing.strokes {
-                    var path = Path()
-                    path.addLines(stroke.points)
+        Canvas { context, size in
+            // drawing here
+            for stroke in drawing.strokes {
+                var path = Path()
+                path.addLines(stroke.points)
 
-                    var contextCopy = context
+                var contextCopy = context
 
-                    if stroke.blur > 0 {
-                        contextCopy.addFilter(.blur(radius: stroke.blur))
-                    }
-
-                    contextCopy.stroke(path, with: .color(stroke.color), style: StrokeStyle(lineWidth: stroke.width, lineCap: .round, lineJoin: .round, dash: [1, stroke.spacing * stroke.width]))
-                }
-            }
-            .gesture(
-                DragGesture(minimumDistance: 0)
-                    .onChanged { value in
-                        drawing.add(point: value.location)
-                    }
-                    .onEnded { _ in
-                        drawing.finishedStroke()
-                    }
-            )
-            .ignoresSafeArea()
-            .navigationTitle("TinyDraw")
-            .toolbar {
-                ToolbarItemGroup(placement: .navigationBarLeading) {
-                    Button(action: drawing.undo) {
-                        Label("Undo", systemImage: "arrow.uturn.backward")
-                    }
-                    .disabled(undoManager?.canUndo == false)
-
-                    Button(action: drawing.redo) {
-                        Label("Redo", systemImage: "arrow.uturn.forward")
-                    }
-                    .disabled(undoManager?.canRedo == false)
+                if stroke.blur > 0 {
+                    contextCopy.addFilter(.blur(radius: stroke.blur))
                 }
 
-                ToolbarItemGroup(placement: .navigationBarTrailing) {
-                    ColorPicker("Color", selection: $drawing.foregroundColor)
-                        .labelsHidden()
-
-                    Button("Brush") {
-                        showingBrushOptions.toggle()
-                    }
-                    .popover(isPresented: $showingBrushOptions) {
-                        LazyVGrid(columns: [.init(.flexible()), .init(.flexible())]) {
-                            Text("Width: \(Int(drawing.lineWidth))")
-                            Slider(value: $drawing.lineWidth, in: 1...100)
-
-                            Text("Softness: \(Int(drawing.blurAmount))")
-                            Slider(value: $drawing.blurAmount, in: 0...50)
-
-                            Text("Spacing: \(drawing.lineSpacing, format: .percent)")
-                            Slider(value: $drawing.lineSpacing, in: 0...5, step: 0.1)
-                        }
-                        .frame(width: 400)
-                        .monospacedDigit()
-                        .padding()
-                    }
-                }
-            }
-            .onAppear {
-                drawing.undoManager = undoManager
+                contextCopy.stroke(path, with: .color(stroke.color), style: StrokeStyle(lineWidth: stroke.width, lineCap: .round, lineJoin: .round, dash: [1, stroke.spacing * stroke.width]))
             }
         }
-        .navigationViewStyle(.stack)
+        .gesture(
+            DragGesture(minimumDistance: 0)
+                .onChanged { value in
+                    drawing.add(point: value.location)
+                }
+                .onEnded { _ in
+                    drawing.finishedStroke()
+                }
+        )
+        .ignoresSafeArea()
+        .toolbar {
+            ToolbarItemGroup(placement: .navigationBarLeading) {
+                Button(action: drawing.undo) {
+                    Label("Undo", systemImage: "arrow.uturn.backward")
+                }
+                .disabled(undoManager?.canUndo == false)
+
+                Button(action: drawing.redo) {
+                    Label("Redo", systemImage: "arrow.uturn.forward")
+                }
+                .disabled(undoManager?.canRedo == false)
+            }
+
+            ToolbarItemGroup(placement: .navigationBarTrailing) {
+                ColorPicker("Color", selection: $drawing.foregroundColor)
+                    .labelsHidden()
+
+                Button("Brush") {
+                    showingBrushOptions.toggle()
+                }
+                .popover(isPresented: $showingBrushOptions) {
+                    LazyVGrid(columns: [.init(.flexible()), .init(.flexible())]) {
+                        Text("Width: \(Int(drawing.lineWidth))")
+                        Slider(value: $drawing.lineWidth, in: 1...100)
+
+                        Text("Softness: \(Int(drawing.blurAmount))")
+                        Slider(value: $drawing.blurAmount, in: 0...50)
+
+                        Text("Spacing: \(drawing.lineSpacing, format: .percent)")
+                        Slider(value: $drawing.lineSpacing, in: 0...5, step: 0.1)
+                    }
+                    .frame(width: 400)
+                    .monospacedDigit()
+                    .padding()
+                }
+            }
+        }
+        .onAppear {
+            drawing.undoManager = undoManager
+        }
     }
 }
 
