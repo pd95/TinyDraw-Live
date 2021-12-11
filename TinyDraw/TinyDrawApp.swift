5695e7d3b548f76e6c5288d46b63b40fc1d18396
Philipp <pd95@users.noreply.github.com>
2021-12-11 19:06:41 +0100

    Initial Commit

---
+++ TinyDraw/TinyDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyDrawApp.swift
+//  TinyDraw
+//
+//  Created by Philipp on 11.12.21.
+//
+
+import SwiftUI
+
+@main
+struct TinyDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
